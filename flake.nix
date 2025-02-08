a693965c32ced92d80fb38f4ec0700b65a8b1847
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-09 00:57:41 +0300

    feat(nix): implement flake
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.clang
+            ];
+            shellHook = ''
+              mkdir -p build
+              cd build
+              cmake ..
+            '';
+          };
+        }
+      );
+    };
+}
